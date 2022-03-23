3230fa114b146cc74fd875d63c2030c26348d25a
Your Name <you@example.com>
2022-03-23 09:00:15 +0600

    added local storage and calculattion all product

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import './Cart.css';
 
 const Cart = ({cart}) => {
-    return (
-        <div>
-             <h2>Order Summary</h2>
-                <p>Selected: {cart.length}</p>
+   let total = 0;
+   let shipping = 0;
+   let quantity = 0;
+   for(const product of cart){
+       quantity = quantity + product.quantity;
+    
+       total = total + product.price * product.quantity;
+       shipping = shipping + product.shipping;
+   }
 
+   const tax = parseFloat((total * 0.1).toFixed(2));
+   const grandTotal = (total + shipping + tax).toFixed(2);
+
+
+
+    return (
+        <div className='cart'>
+             <h2>Order Summary</h2>
+               <div className='cart-calculation'>
+               <p>Selected Items: {quantity}</p>
+                <p>Total Price: ${total}</p>
+                <p>Total Shipping: ${shipping}</p>
+                <p>Tax: ${tax}</p>
+                <h4>Grand Total: ${grandTotal}</h4>
+               </div>
         </div>
     );
 };
